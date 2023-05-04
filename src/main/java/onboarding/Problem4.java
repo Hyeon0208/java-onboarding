83b33f15155c687e32d723239ab7d228fb5aa812
SuhHyeonjun <dmsgktn0208@gmail.com>
2023-05-04 12:48:56 +0900

    feat : 청개구리 사전

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -2,7 +2,24 @@ package onboarding;
 
 public class Problem4 {
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            answer.append(convertReverse(ch));
+        }
+        return answer.toString();
+    }
+
+    private static char convertReverse(char ch) {
+        if (ch == ' ') {
+            return ch;
+        }
+        if (Character.isLowerCase(ch)) {
+            return (char)('z' - (ch - 'a'));
+        }
+        if (Character.isUpperCase(ch)) {
+            return (char)('Z' - (ch - 'A'));
+        }
+        return ch;
     }
 }
